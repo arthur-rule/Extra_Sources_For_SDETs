3a9c6883d57a17ead8b77b335fc34effb4ba7069
Arthur <105047736+arthur-rule@users.noreply.github.com>
2023-11-29 00:09:59 +0300

    First commit declarationInstantiationInitialization

---
+++ src/Java/Extra_Sources/declarationInstantiationInitialization.java
@@ -0,0 +1,46 @@
+package Java.Extra_Sources;
+
+public class declarationInstantiationInitialization {
+    public static void main(String[] args) {
+        /*In Java, declaration, instantiation, and initialization are three key
+        concepts related to working with variables.
+        1.Declaration:
+
+        This is when you tell the compiler about a variable's name and type
+         but don't allocate any memory for it. Essentially, you're saying,
+         "Hey, I'm going to use this variable, and it's going to be of this type,
+         but it does not allocate memory or assign a value to the variable.*/
+
+        int myNumber;
+
+        /*
+        2.Instantiation:
+        This term is often used in the context of object-oriented programming.
+        Instantiation is the creation of an object or an instance of a class.
+        It involves using the new keyword followed by a constructor to create
+        an object in memory.*/
+
+
+
+        /*
+        3.Initialization:
+        Initialization is the process of assigning an initial value to a variable.
+        It can occur at the time of declaration or later in the code.*/
+
+        int myNumber2 = 42; // Initialization at the time of declaration
+        int anotherNumber; // Declaration
+        anotherNumber = 55; // Initialization later in the code
+
+        /*In summary:
+        Declaration is like telling the program, "I'm going to use this variable."
+        Instantiation is creating an instance of a class or an object.
+        Initialization is giving a variable its initial value.*/
+
+
+
+
+
+
+
+    }
+}
