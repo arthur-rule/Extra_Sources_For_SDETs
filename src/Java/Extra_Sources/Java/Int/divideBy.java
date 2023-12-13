e203b4943cdee7ab6492917683cdeb792bf836ad
Arthur <105047736+arthur-rule@users.noreply.github.com>
2023-12-13 14:43:27 +0300

    12/13/23

---
+++ src/Java/Extra_Sources/Java/Int/divideBy.java
@@ -0,0 +1,9 @@
+package Java.Extra_Sources.Java.Int;
+
+public class divideBy {
+    public static void main(String[] args) {
+        int num1=9;
+        System.out.println((num1 / 2));
+    }
+
+}
