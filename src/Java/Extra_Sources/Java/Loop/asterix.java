346450d7eac92e600414029bcc570d2b2f2c1b0e
Arthur <105047736+arthur-rule@users.noreply.github.com>
2023-12-19 01:05:00 +0300

    12/19/23

--- src/Java/Extra_Sources/Java/Loop/asterix.java
+++ src/Java/Extra_Sources/Java/Loop/asterix.java
@@ -2,7 +2,10 @@ package Java.Extra_Sources.Java.Loop;
 
 public class asterix {
     public static void main(String[] args) {
-        for (int i =1; i<=10; i++){
+
+
+
+        for (int i =1; i<=5; i++){
             for (int j=1;j<=i;j++){
 
                 System.out.print("*");
