f263864bba1caa1d4d370bdf6bcf68ba8405c1da
Arthur <105047736+arthur-rule@users.noreply.github.com>
2023-12-27 00:10:33 +0300

    12/27/23

--- src/Java/Extra_Sources/Java/Loop/fiveLapsThreePush_Ups.java
+++ src/Java/Extra_Sources/Java/Loop/fiveLapsThreePush_Ups.java
@@ -4,6 +4,7 @@ public class fiveLapsThreePush_Ups {
 
     public static void main(String[] args) {
 
+
         /* Lets think about an athlete. A coach asks a runner athlete to perform two tasks simultaneously. The first task is to run 5 laps around the track. The second task is to do 3 push-ups after completing each lap before starting the next one. The athlete starts running from the starting point, finishes the first lap, does 3 push-ups, and starts the second lap. They continue in this manner, doing 3 push-ups after each lap, and complete 5 laps. This example beautifully illustrates the concept of nested loops in Java.*/
 
 
