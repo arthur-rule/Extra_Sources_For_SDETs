7706c166bad0e25febd18ea03c8782bd9620a3d5
Arthur <105047736+arthur-rule@users.noreply.github.com>
2023-12-27 00:17:42 +0300

    12/27/23

---
+++ src/Java/Extra_Sources/Java/Array_Arraylist/qa.java
@@ -0,0 +1,13 @@
+package Java.Extra_Sources.Java.Array_Arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class qa {
+    public static void main(String[] args) {
+        int[]arr={0,0,0,0};
+        int[]arr2=new int[4];
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr2));
+    }
+}
