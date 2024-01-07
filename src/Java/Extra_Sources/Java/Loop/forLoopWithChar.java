cecfa03993acc90dde7d64b8c0e7d92476b1c370
Arthur <105047736+arthur-rule@users.noreply.github.com>
2024-01-07 23:10:18 +0300

    01/07/23

--- src/Java/Extra_Sources/Java/Loop/forLoopWithChar.java
+++ src/Java/Extra_Sources/Java/Loop/forLoopWithChar.java
@@ -2,21 +2,32 @@ package Java.Extra_Sources.Java.Loop;
 
 public class forLoopWithChar {
     public static void main(String[] args) {
+
+        System.out.println("Iteration with int");
         for(int i=0; i<3; i++){
             System.out.println("i = " + i);
         }
+        System.out.println("---------------------------------------------------");
 
-        for(char j=65; j<68; j++){
-            System.out.println("j = " + j);
+        System.out.println("Iteration with char");
+        for(char i= 'A'; i<'D'; i++){
+            System.out.println("i = " + i);
+        }
+        System.out.println("---------------------------------------------------");
+        System.out.println("Iteration with char and printing that char's decimal value with casting it to int. Casting is when we explicitly convert from one primitve data type to another.");
+
+        for(char i ='A'; i<'D' ; i++) {
+            System.out.println("i = " + (int) i);
+        }
+        System.out.println("---------------------------------------------------");
+
+        System.out.println("Iteration with directly decimal value of char");
+        for(char i=65; i<68; i++){
+            System.out.println("i = " + (int)i);
         }
 
-        for(char k= 'a'; k<'d'; k++){
-            System.out.println("k = " + k);
-        }
 
-        for(char m ='a'; m<'d' ; m++){
-            System.out.println("m = " + (int) m);
-        }
+
 
     }
 }
