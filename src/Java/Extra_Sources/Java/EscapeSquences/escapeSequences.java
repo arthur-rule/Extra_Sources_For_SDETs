730f190b2b0a83fa2b618bcf726cf0185cc0f21d
Arthur <105047736+arthur-rule@users.noreply.github.com>
2024-01-09 13:58:19 +0300

    01/09/23

---
+++ src/Java/Extra_Sources/Java/EscapeSquences/escapeSequences.java
@@ -0,0 +1,30 @@
+package Java.Extra_Sources.Java.EscapeSquences;
+
+public class escapeSequences {
+            public static void main(String[] args) {
+            // Newline
+            System.out.println("Hello\nWorld");
+
+            // Tab
+            System.out.println("Name:\tJohn");
+
+            // Backspace
+            System.out.println("Hello\bWorld");
+
+            // Carriage Return
+            System.out.println("Hello\rWorld");
+
+            // Backslash
+            System.out.println("This is a backslash: \\");
+
+            // Single Quote
+            System.out.println("It\'s a sunny day.");
+
+            // Double Quote
+            System.out.println("She said, \"Hello!\"");
+
+            // Unicode character
+            System.out.println("Greek Omega: \u03A9");
+        }
+    }
+
