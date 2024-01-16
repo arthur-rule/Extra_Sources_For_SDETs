5c84ec5440ea1551254b0028a58db67d88418e25
Arthur <105047736+arthur-rule@users.noreply.github.com>
2024-01-16 23:26:55 +0300

    01/16/23

---
+++ src/extra_sources/java/return_exit/ReturnVsExit.java
@@ -0,0 +1,20 @@
+package extra_sources.java.return_exit;
+
+public class ReturnVsExit {
+    public static void main(String[] args) {
+
+        System.out.println("Test Started");
+
+        methodA();
+
+        System.out.println("Test Completed");
+
+    }
+
+
+    public static void methodA(){
+        System.out.println("Method A is being executed");
+        return;
+        //System.exit(0);
+    }
+}
