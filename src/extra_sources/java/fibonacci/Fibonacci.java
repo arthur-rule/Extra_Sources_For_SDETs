65da3eeb399dc129c1ced78e732f1e21f0946b35
Arthur <105047736+arthur-rule@users.noreply.github.com>
2024-02-13 22:39:07 +0300

    02/07/24

---
+++ src/extra_sources/java/fibonacci/Fibonacci.java
@@ -0,0 +1,24 @@
+package extra_sources.java.fibonacci;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        System.out.println(fibonacci(4));
+
+    }
+
+    public static int fibonacci(int n) {
+
+  /*Recursion in Java refers to the process in which a method calls itself either
+  directly or indirectly to solve a problem. In a recursive method, the solution
+  to a problem is expressed in terms of simpler instances of the same problem.
+  The base case(s) defines the condition under which the recursion stops, preventing
+  an infinite loop.*/
+
+        if(n==0 ) return 0;
+        if( n==1) return 1;
+        return fibonacci(n-1)+fibonacci(n-2);
+
+    }
+
+}
