c08b0998c977acecba5583c5c69c5343deb68104
arthur-rule <arthur.rule@cydeo.com>
2024-05-08 23:21:41 +0300

    03/21/2024

--- src/extraSources/java/return_exit/ReturnVsExit.java
+++ src/extraSources/java/return_exit/ReturnVsExit.java
@@ -11,7 +11,7 @@ public class ReturnVsExit {
 
     }
     public static void methodA(){
-        System.out.println("Method A is being executed");
+        System.out.println("method A is being executed");
         return;
         //System.exit(0);
     }
